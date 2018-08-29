3ba87268eb5749f49239157a4876eb8370f8d44f
KraIsrael <jacob.kra@inpulsion.net>
2018-08-29 10:53:10 +0000

    database config

--- config/database.php
+++ config/database.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    #'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'online',
 
     /*
     |--------------------------------------------------------------------------
@@ -46,6 +47,20 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+        'online' => [
+            'driver' => 'mysql',
+            'host' => 'vps548257.ovh.net',
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'c0facebookpage',
+            'username' => 'c0usrinpulsclic',
+            'password' => 'syogXBH#37',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
