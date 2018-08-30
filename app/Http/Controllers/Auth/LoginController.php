9b4daf122cc1f9b3c5e97315da1e063ad4264226
KraIsrael <jacob.kra@inpulsion.net>
2018-08-30 14:59:39 +0000

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -69,12 +69,12 @@ class LoginController extends Controller
             $fb = new Facebook(array('app_id'=>env('FACEBOOK_CLIENT_ID'),'app_secret'=>env('FACEBOOK_CLIENT_SECRET')));
             $page = $fb->get('/me/accounts',$user->token);
            // $page = $fb->getDecodeBody();
-            $test = $fb->get('/me/pages',$user->token);
+           // $test = $fb->get('/me/pages',$user->token);
 
         }catch (FacebookSDKException $exception){
             dd($exception);
         }
-        dd(array('page'=>$page, 'msg'=>'message','test'=>$test));
+        dd($page);
         $user = User::updateOrCreate(
             [
                 'email' => $user->email
